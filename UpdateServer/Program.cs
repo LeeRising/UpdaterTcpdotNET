93c4034c32b3c801af6255fa2fbc7a6757194ec4
LeeRising <leerising773@gmail.com>
2017-06-19 12:58:04 +0300

    Add file to Git

---
+++ UpdateServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UpdateServer
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+
+        }
+    }
+}
